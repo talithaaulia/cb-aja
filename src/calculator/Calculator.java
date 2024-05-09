e13e6fdea1be5945dde138065e12dd6ab9c9573b
firohazzah <dhofiroh@student.telkomuniversity.ac.id>
2024-05-09 12:28:20 +0700

    first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author Dhofiroh Azzah
+ */
+public class Calculator extends demo{
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+    // memanggil method dengan 2 parameter
+        calculator.add(10, 20); // output: 30
+    // memanggil method dengan 3 parameter
+        calculator.add(10, 20, 30); // output: 60
+    }
+
+  
+
+}
+
+
+
+
+
+ 
+
+    
+
---
+++ src/calculator/demo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author Dhofiroh Azzah
+ */
+class demo {
+ void add(int a,int b) {
+ System.out.println(a+b);
+ }
+ void add(int a, int b, int c) {
+ System.out.println(a+b+c);
+ }
+}
+    
+
